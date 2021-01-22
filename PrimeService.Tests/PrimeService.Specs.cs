60a8316572dbda93858e92646a29ff7420195de4
Grant <gdblackx@me.com>
2021-01-22 17:03:18 -0500

    finished building out the class and tests

--- PrimeService.Tests/PrimeService.Specs.cs
+++ PrimeService.Tests/PrimeService.Specs.cs
@@ -8,5 +8,30 @@ namespace PrimeService.Tests
     // Worth mentioning MS used this naming convention we can use something that make more sense to us!
     public class PrimeService_IsPrime_Should
     {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrime_Should()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            Assert.IsFalse(_primeService.IsPrime(1));
+        }
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(3)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+        
     }
 }
---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeService
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
