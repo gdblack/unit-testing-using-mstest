17b82d3dc90f36662ddb01b57a433d02ea0b2125
Grant <gdblackx@me.com>
2021-01-22 11:41:12 -0500

    initial commit

---
+++ PrimeService.Tests/PrimeService.Specs.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PrimeService.Tests
+{
+    [TestClass]
+    // Establish a naming convention - the test should show it's intent
+    // Worth mentioning MS used this naming convention we can use something that make more sense to us!
+    public class PrimeService_IsPrime_Should
+    {
+    }
+}
